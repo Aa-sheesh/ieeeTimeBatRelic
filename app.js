6fc7d70137a1907d614e62b070758977837defae
Aa-sheesh <aashish.229301585@muj.manipal.edu>
2023-07-01 21:19:46 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,29 @@
+//jshint esversion:6
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+var _ = require('lodash');
+const app = express();
+
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
+
+app.get("/",function(req,res){
+  const options = {};
+  res.render("home.ejs", options);
+  console.log(req.body.name);
+})
+app.post("/", function(req,res) {
+  res.render("login.ejs");
+})
+app.get("/signup",function(req,res){
+  const options = {};
+  res.render("login.ejs", options);
+})
+
+app.listen(3000, function() {
+    console.log("Server is up and running at port 3000!");
+})
